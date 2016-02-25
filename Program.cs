be8f059325b22d81d41f4cfc3c1c6931b4252e1b
sthakuri <suvashshah.thakuri@gmail.com>
2016-02-25 09:23:08 -0800

    Folder Re-structure

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using RssFeedReader.Channel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RssFeedReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RssFeedReader.Model.FeedReader reader = new Model.FeedReader();
+            reader.Add(new OnlineKhabar());
+            Console.WriteLine(reader.ToString());
+            Console.ReadLine();            
+        }
+    }
+}
